a3e2762f2e4d1536bb0cc67967bf12ad6eba0f10
William R. Fraser <wfraser@codewise.org>
2014-09-06 14:44:56 -0700

    oops, forgot to add these to commit 21c3364e5cc46f89368d46d38abf2c33116f1e6e

---
+++ Controls/BatteryStatusControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using System.Windows.Threading;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Breadcrumbs
+{
+    public partial class BatteryStatusControl : UserControl
+    {
+        public BatteryStatusControl()
+        {
+            InitializeComponent();
+            m_viewModel = new ViewModels.BatteryViewModel();
+            m_viewModel.Start();
+            DataContext = m_viewModel;
+        }
+
+        ~BatteryStatusControl()
+        {
+            m_viewModel.Stop();
+        }
+
+        ViewModels.BatteryViewModel m_viewModel;
+    }
+}
